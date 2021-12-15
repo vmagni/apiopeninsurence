500a106ab85d2fc450c723a3b7bb06caca45a2df
Victor Hugo Magni Daniel <victor.magni@foursys.com.br>
2021-12-15 19:08:30 -0300

    ajuste rota

--- ApiOpenInsurence/Caixa.OpenInsurence.Api/Controllers/ChannelsController.cs
+++ ApiOpenInsurence/Caixa.OpenInsurence.Api/Controllers/ChannelsController.cs
@@ -248,7 +248,7 @@ namespace Caixa.OpenInsurence.Api.Controllers
         /// Retorna os canais de atendimento telefônico.
         /// </summary>
         [HttpPost]
-        [Route("Phone")]
+        [Route("Phones")]
         [ProducesResponseType(typeof(BranchChannelResponse), 200)] //OK
         [ProducesResponseType(typeof(ResponseError), 400)] //OK
         [ProducesResponseType(typeof(ResponseError), 401)]
